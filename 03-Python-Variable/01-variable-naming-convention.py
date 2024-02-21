3b3a11e2c085603c9bf719fa29fb7c8e0986cd91
Manish Pandey <57837808+learningwithmainsh@users.noreply.github.com>
2024-02-21 06:15:29 +0000

    adding some variable example

---
+++ 03-Python-Variable/01-variable-naming-convention.py
@@ -0,0 +1,12 @@
+# Good variable naming
+user_name = "John"
+total_items = 42
+
+# Avoid using reserved words
+# class = "Python"  # Not recommended
+
+# Use meaningful names
+a = 10  # Less clear
+num_of_students = 10  # More descriptive
+
+print(user_name,total_items,a,num_of_students)
